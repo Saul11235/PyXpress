0ba9a25b3498fd6e3277977d481ad43bb45c10ff
Edwin Saul <edwinsaulpm@gmail.com>
2021-12-29 23:07:31 -0500

    creado ejemplos sencillos

---
+++ PyXpress.py
@@ -0,0 +1,11 @@
+#Program for speed presentations whit a text file
+#programa para hacer presentaciones sencillas desde un archivo de texto 
+#
+# programado por: Saul11235 
+#
+# For customize settings see the options in -->config.py
+# Para modificar las opciones edita el archivo -->config.py
+
+
+
+
